946dc27493a3e70d52defc85658471ef8a5884b5
Adriano Mourao <mourao.aaa@gmail.com>
2024-06-22 18:45:11 -0300

    Adding model

---
+++ src/Model.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Adriano Mourão on 22/06/24.
+//
+
+#include <iloenv.h>
+#include "Model.h"
+
+Model::Model() {
+    IloEnv env;
+}
---
+++ src/Model.h
@@ -0,0 +1,16 @@
+//
+// Created by Adriano Mourão on 22/06/24.
+//
+
+#ifndef NETWORKFLOWSTF_MODEL_H
+#define NETWORKFLOWSTF_MODEL_H
+
+
+class Model {
+public:
+    Model();
+
+};
+
+
+#endif //NETWORKFLOWSTF_MODEL_H
